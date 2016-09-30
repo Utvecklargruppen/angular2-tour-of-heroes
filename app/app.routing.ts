16d5d22eb4279dca0aaa32853a93fc91cb729047
Jan-Olof Nilsson <janolof@gmail.com>
2016-09-30 14:57:41 +0200

    Routing, continued...

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }      from './hero-detail.component';
 import { DashboardComponent }      from './dashboard.component';
 
 const appRoutes: Routes = [
@@ -17,7 +18,12 @@ const appRoutes: Routes = [
 {
     path: 'heroes',
     component: HeroesComponent
-  }
+  },
+  {
+  path: 'detail/:id',
+  component: HeroDetailComponent
+},
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
     selector: 'my-hero-detail',
-    template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`
+  templateUrl: 'app/hero-detail.component.html'
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
     @Input()
     hero: Hero;
+
+    constructor(
+  private heroService: HeroService,
+  private route: ActivatedRoute,
+  private location: Location
+) {}
+
+ngOnInit(): void {
+  this.route.params.forEach((params: Params) => {
+    let id = +params['id'];
+    this.heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  });
+}
+
+goBack(): void {
+  this.location.back();
+}
+
 }
